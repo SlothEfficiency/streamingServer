27796c32e129278039bb5db86b1fa36c923432fd
Pascal Bothe <pascalbothe@gmx.de>
2026-09-04 21:06:09 +0200

    in stream umwandenln

--- main.go
+++ main.go
@@ -32,17 +32,19 @@ func main() {
 		return
 	}
 
-	// Take a picture
-	frame, err := nextFrame(cam, timeout)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	for {
+		// Take a picture
+		frame, err := nextFrame(cam, timeout)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-	//Save the picture
-	err = os.WriteFile("frame.jpeg", frame, 0666)
-	if err != nil {
-		log.Println(err)
-		return
+		//Save the picture
+		err = os.WriteFile("frame.jpeg", frame, 0666)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
 }
